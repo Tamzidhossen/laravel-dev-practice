fa39b4f26f2d8af5f92ad81be21866ca43c6ffef
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-04-03 21:47:50 +0600

    Updated

--- app/Http/Middleware/CategoryMiddleware.php
+++ app/Http/Middleware/CategoryMiddleware.php
@@ -15,13 +15,13 @@ class CategoryMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $key = $request->header('secret-key');  //Request Header
+        /*$key = $request->header('secret-key');  //Request Header
         $Body = $request->input('stock');       //Request Body
         $key = $request->key;                   //Request Parameter
         if ($key != 'key' || $Body !== 7) {
             return response()->json(['message' => 'Unauthorized'], 401);
-        }
-        // $request->headers->add(['data' => 'new-value']);  //Request Header Add
+        }*/
+        $request->headers->add(['data' => 'new-value']);  //Request Header Add
         //$request->headers->replace(['data' => 'Replace Value']);  //Request Header Replace
         return $next($request);
     }
