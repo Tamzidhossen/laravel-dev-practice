08efbe98fd2aa8119152fd41b882d5e8645dffab
Tamzid Hossen <tamzidhossen366@gmail.com>
2026-04-06 22:01:46 +0600

    Create MakeTrait

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DemoController extends Controller
 {
+    public function __construct()
+    {
+        echo "Constructor Executed". PHP_EOL;
+    }
+
     public function Demo1(): JsonResponse
     {
         return response()->json(['Message' => 'Demo1 Executed'], 201);
